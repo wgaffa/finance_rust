d872df7b829aa007ea750c33ded50274ebbf70bc
Patrik Maunus <subscription@skriptladan.se>
2021-07-23 19:22:07 +0200

    Promote Debit/Credit to types

--- src/bookkeeping.rs
+++ src/bookkeeping.rs
@@ -1,74 +1,54 @@
+use std::marker::PhantomData;
+
 /// Entry type for bookkeeping
 #[derive(Debug, PartialEq)]
-pub enum Entry {
-    Debit,
-    Credit,
+pub struct Debit;
+
+#[derive(Debug, PartialEq)]
+pub struct Credit;
+
+/// Data for a single transaction holding the entry type and amount
+pub struct Transaction<T> {
+    amount: u32,
+    phantom: PhantomData<T>,
 }
 
-type AmountType = Amount;
-
-#[derive(Debug)]
-pub struct Amount(i32);
-
-impl Amount {
-    pub fn new(amount: i32) -> Self {
-        // NOTE: abs() does not panic in release mode for i32::MIN, instead it returns i32::MIN
-        Self (amount.abs())
+impl<T> Transaction<T> {
+    pub fn amount(&self) -> u32 {
+        self.amount
     }
 }
 
-/// Data for a single transaction holding the entry type and amount
-pub struct Transaction {
-    entry: Entry,
-    amount: AmountType,
-}
-
-impl Transaction {
+impl Transaction::<Debit> {
     /// Create a new debit transaction
     ///
     /// ```
-    /// use personal_finance::bookkeeping::{Transaction, Amount};
-    /// let transaction = Transaction::debit(Amount::new(40));
+    /// use personal_finance::bookkeeping::Transaction;
+    /// let transaction = Transaction::debit(40);
     /// assert_eq!(transaction.amount(), 40);
     /// ```
-    pub fn debit(amount: AmountType) -> Self {
+    pub fn debit(amount: u32) -> Self {
         Self {
-            entry: Entry::Debit,
             amount,
-        }
-    }
-
-    /// Create a new credit transaction
-    ///
-    /// ```
-    /// use personal_finance::bookkeeping::{Transaction, Amount};
-    /// let transaction = Transaction::credit(Amount::new(70));
-    /// assert_eq!(transaction.amount(), 70);
-    /// ```
-    pub fn credit(amount: AmountType) -> Self {
-        Self {
-            entry: Entry::Credit,
-            amount,
-        }
-    }
-
-    pub fn amount(&self) -> i32 {
-        self.amount.0
-    }
-
-    pub fn entry(&self) -> &Entry {
-        &self.entry
-    }
-
-    pub fn to_numeral(&self) -> i32 {
-        self.amount.0 * match self.entry {
-            Entry::Debit => 1,
-            Entry::Credit => -1,
+            phantom: PhantomData,
         }
     }
 }
 
-fn out_of_debt(e: Entry) {
+impl Transaction::<Credit> {
+    /// Create a new credit transaction
+    ///
+    /// ```
+    /// use personal_finance::bookkeeping::Transaction;
+    /// let transaction = Transaction::credit(70);
+    /// assert_eq!(transaction.amount(), 70);
+    /// ```
+    pub fn credit(amount: u32) -> Self {
+        Self {
+            amount,
+            phantom: PhantomData,
+        }
+    }
 }
 
 #[cfg(test)]
--- src/bookkeeping_tests.rs
+++ src/bookkeeping_tests.rs
@@ -2,43 +2,31 @@
 use super::*;
 
 use test_case::test_case;
+use std::any::{Any, TypeId};
 
-#[test]
-fn new_debit_test() {
-    let actual = Transaction::debit(Amount::new(500));
+fn is_debit_transaction<T: ?Sized + Any>(_t : &T) -> bool {
+    TypeId::of::<Transaction<Debit>>() == TypeId::of::<T>()
+}
 
-    assert_eq!(actual.entry, Entry::Debit);
-    assert_eq!(actual.amount.0, 500);
+fn is_credit_transaction<T: ?Sized + Any>(_t : &T) -> bool {
+    TypeId::of::<Transaction<Credit>>() == TypeId::of::<T>()
 }
 
 #[test_case(100, 100)]
-#[test_case(-300, 300)]
-#[test_case(i32::MIN, 5 => panics "attempt to negate with overflow")]
-fn new_credit_test(amount: i32, expected: i32) {
-    let actual = Transaction::credit(Amount::new(amount));
+#[test_case(u32::MAX, 4294967295)]
+fn new_debit_test(amount: u32, expected: u32) {
+    let actual = Transaction::debit(amount);
 
-    assert_eq!(actual.entry, Entry::Credit);
-    assert_eq!(actual.amount.0, expected);
+    assert!(is_debit_transaction(&actual));
+    assert_eq!(actual.amount, expected);
 }
 
-#[test_case(Amount::new(100), 100)]
-#[test_case(Amount::new(i32::MAX), i32::MAX)]
-#[test_case(Amount::new(-200), 200)]
-fn debit_to_numeral(amount: AmountType, expected: i32) {
-    let transaction = Transaction::debit(amount);
+#[test_case(100, 100)]
+#[test_case(u32::MAX, 4294967295)]
+fn new_credit_test(amount: u32, expected: u32) {
+    let actual = Transaction::credit(amount);
 
-    let actual = transaction.to_numeral();
-
-    assert_eq!(actual, expected);
+    assert!(is_credit_transaction(&actual));
+    assert_eq!(actual.amount, expected);
 }
 
-#[test_case(Amount::new(100), -100)]
-#[test_case(Amount::new(i32::MAX), -i32::MAX)]
-#[test_case(Amount::new(-200), -200)]
-fn credit_to_numeral(amount: AmountType, expected: i32) {
-    let transaction = Transaction::credit(amount);
-
-    let actual = transaction.to_numeral();
-
-    assert_eq!(actual, expected);
-}
