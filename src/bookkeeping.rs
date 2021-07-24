0a382fe21c4b3485b5ce2e84c2e0258435510b40
Patrik Maunus <subscription@skriptladan.se>
2021-07-24 16:07:44 +0200

    Not perfect but experimental

--- src/bookkeeping.rs
+++ src/bookkeeping.rs
@@ -1,14 +1,30 @@
+use std::any::Any;
 use std::iter::Sum;
 use std::marker::PhantomData;
 
-/// Entry type for bookkeeping
+pub enum Entry {
+    Debit,
+    Credit,
+}
+
 #[derive(Debug, PartialEq)]
 pub struct Debit;
 
 #[derive(Debug, PartialEq)]
 pub struct Credit;
 
+pub trait TransactionMarker: Any {
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl<T: 'static> TransactionMarker for Transaction<T> {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
 /// Data for a single transaction holding the entry type and amount
+#[derive(Debug, PartialEq)]
 pub struct Transaction<T> {
     amount: u32,
     phantom: PhantomData<T>,
@@ -84,6 +100,24 @@ impl<'a, T> Sum for Transaction<T> {
             phantom: PhantomData,
         })
     }
+}
+
+pub fn split(collection: Vec<Box<dyn Any>>) -> (Vec<Transaction<Debit>>, Vec<Transaction<Credit>>) {
+    let (debits, credits): (Vec<Box<dyn Any>>, Vec<Box<dyn Any>>) =
+        collection
+        .into_iter()
+        .partition(|x| x.as_ref().is::<Transaction<Debit>>());
+
+    let debits = debits
+        .into_iter()
+        .map(|x| *x.downcast::<Transaction<Debit>>().unwrap())
+        .collect::<Vec<Transaction<Debit>>>();
+    let credits = credits
+        .into_iter()
+        .map(|x| *x.downcast::<Transaction<Credit>>().unwrap())
+        .collect::<Vec<Transaction<Credit>>>();
+
+    (debits, credits)
 }
 
 #[cfg(test)]
