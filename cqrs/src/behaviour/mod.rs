c048a72e892e1f5c6fafdff3b497aa127c72dd42
Patrik Maunus <subscription@skriptladan.se>
2022-08-21 16:35:17 +0200

    Fix error message

--- cqrs/src/behaviour/mod.rs
+++ cqrs/src/behaviour/mod.rs
@@ -9,7 +9,7 @@ pub enum AccountError {
 impl std::fmt::Display for AccountError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::AccountAlreadyOpened(id) => write!(f, "Account '{id}' is already open"),
+            Self::AccountAlreadyOpened(id) => write!(f, "Account '{id}' has already been opened"),
         }
     }
 }
