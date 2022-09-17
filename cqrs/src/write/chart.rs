bc1da93dba8f7ba9f6e236d5e5e54f10c7561e90
Patrik Maunus <subscription@skriptladan.se>
2022-09-17 17:25:41 +0200

    refactor: Move structs into it's own modules

---
+++ cqrs/src/write/chart.rs
@@ -0,0 +1,62 @@
+use std::collections::HashSet;
+
+use personal_finance::account::{Category, Name, Number};
+
+use crate::{AccountError, AccountId, Event};
+
+#[derive(Default)]
+pub struct Chart {
+    data: HashSet<AccountId>,
+    history: Vec<Event>,
+}
+
+impl Chart {
+    pub fn new(history: &[Event]) -> Self {
+        let mut chart = Self {
+            data: Default::default(),
+            history: history.to_vec(),
+        };
+
+        chart.apply(history);
+
+        chart
+    }
+
+    pub fn open(
+        &mut self,
+        number: Number,
+        name: Name,
+        category: Category,
+    ) -> Result<&[Event], AccountError> {
+        let account_doesnt_exist = !self.data.contains(&number.number());
+        account_doesnt_exist
+            .then(|| (number.number(), name.into_inner(), category))
+            .map(|(id, name, category)| vec![Event::AccountOpened { id, name, category }])
+            .map(|issued_events| {
+                let len = issued_events.len();
+                self.apply(&issued_events);
+                self.history.extend(issued_events);
+
+                len
+            })
+            .map(|len| {
+                let index = self.history.len().checked_sub(len).unwrap_or_default();
+                &self.history[index..]
+            })
+            .ok_or_else(|| AccountError::AccountAlreadyOpened(number.number()))
+    }
+
+    fn apply(&mut self, events: &[Event]) {
+        for event in events {
+            match event {
+                Event::AccountOpened { id, .. } => {
+                    self.data.insert(*id);
+                }
+                Event::AccountClosed(id) => {
+                    self.data.remove(id);
+                }
+                _ => {}
+            }
+        }
+    }
+}
---
+++ cqrs/src/write/journal.rs
@@ -0,0 +1,102 @@
+use std::{collections::HashSet, ops::Neg};
+
+use crate::{AccountId, Balance, Event, JournalError, JournalId};
+
+pub struct Journal {
+    current_id: JournalId,
+    accounts: HashSet<AccountId>,
+    history: Vec<Event>,
+}
+
+fn transcribe_amount(amount: Balance) -> i64 {
+    match amount {
+        Balance::Debit(x) => i64::from(x),
+        Balance::Credit(x) => i64::from(x).neg(),
+    }
+}
+
+fn make_journal(
+    id: JournalId,
+    description: String,
+    transactions: &[(AccountId, Balance)],
+) -> Vec<Event> {
+    let mut v = vec![Event::Journal {
+        id,
+        description: description.into(),
+    }];
+    v.extend(
+        transactions
+            .iter()
+            .map(|(account, amount)| Event::Transaction {
+                account: *account,
+                amount: *amount,
+                journal: id,
+            }),
+    );
+
+    v
+}
+
+fn next_id(current: JournalId) -> Result<JournalId, JournalError> {
+    current
+        .checked_add(1)
+        .ok_or(JournalError::JournalLimitReached)
+}
+
+impl Journal {
+    pub fn new(history: &[Event]) -> Self {
+        let mut journal = Self {
+            current_id: 0,
+            accounts: HashSet::new(),
+            history: history.to_vec(),
+        };
+
+        journal.apply(history);
+
+        journal
+    }
+
+    pub fn entry<T: Into<String>>(
+        &mut self,
+        description: T,
+        transactions: &[(AccountId, Balance)],
+    ) -> Result<&[Event], JournalError> {
+        transactions
+            .len()
+            .gt(&0)
+            .then_some(())
+            .ok_or(JournalError::EmptyTransaction)
+            .and_then(|()| {
+                transactions
+                    .iter()
+                    .fold(0, |sum, (_, amount)| sum + transcribe_amount(*amount))
+                    .eq(&0)
+                    .then_some(())
+                    .ok_or(JournalError::ImbalancedTranasactions)
+                    .and_then(|()| next_id(self.current_id))
+                    .map(|id| make_journal(id, description.into(), &transactions))
+                    .map(|events| {
+                        self.apply(&events);
+                        let len = self.history.len();
+                        self.history.extend(events);
+                        len
+                    })
+                    .map(|len| &self.history[len..])
+            })
+    }
+
+    fn apply(&mut self, events: &[Event]) {
+        for event in events {
+            match event {
+                Event::AccountOpened { id, .. } => {
+                    self.accounts.insert(*id);
+                }
+                Event::AccountClosed(id) => {
+                    self.accounts.remove(id);
+                }
+                Event::Journal { id, .. } => self.current_id = self.current_id.max(*id),
+                _ => {}
+            }
+        }
+    }
+}
